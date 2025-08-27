1bb600791e2471714fb9004fa9be8698705a2086
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-08-26 23:09:15 -0300

    feat:implementado funcoes para envio de mensagem a todos conectados no servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,10 @@ const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
 
 wss.on("connection", (ws) => {
     ws.on("error", console.error);
-    ws.on("message",(data)=>{
-        wss.clients.forEach((client)=>{
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => {
             client.send(data.toString())
-        }) 
+        })
     })
     console.log("Client connected");
 })
--- frontend/script.js
+++ frontend/script.js
@@ -7,8 +7,48 @@ const chatForm = document.querySelector(".chat__form")
 const chatInput = document.querySelector(".chat__input")
 const chatMessage = document.querySelector(".chat__message")
 
-const handleSubmit = (event) => {
-    event.preventDefault()
+const colors = [
+    "aqua",
+    "cadetblue",
+    "blueviolet",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold",
+    "navy"
+]
+let websocket
+
+const user = { id: "", name: "", color: "" } /* objeto do usuario */
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
 }
 
+const processMessage = ({ data }) => {
+    console.log(data)
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+}
+
+const sendMessage = (event) => {
+    event.preventDefault()
+    websocket.send(chatInput.value)
+}
+
+
 loginForm.addEventListener("submit", handleSubmit)
+chatForm.addEventListener("submit", sendMessage)
